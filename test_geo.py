4183248180be4c961be5f3abdd3c62c50ef30b1a
J W Cassidy <jwc63@cam.ac.uk>
2026-02-08 17:25:32 +0000

    tests for B,C&F - JC

---
+++ test_geo.py
@@ -0,0 +1,33 @@
+from floodsystem import geo
+from floodsystem.station import MonitoringStation
+
+def test_stations_by_distance():
+    """Tests stations_by_distance"""
+
+    A = MonitoringStation('idA', 'measureA', 'A', (0, 0), (0, 1), 'riverA', 'townA')
+    B = MonitoringStation('idB', 'measureB', 'B', (1, 1), (0, 1), 'riverB', 'townB')
+    C = MonitoringStation('idC', 'measureC', 'C', (-2, -2), (0, 1), 'riverC', 'townC')
+
+    stations = [A, B, C]
+    p = (0, 0)
+
+    result = geo.stations_by_distance(stations, p)
+    assert result[0][0] == A
+    assert result[1][0] == B
+    assert result[2][0] == C
+
+def test_stations_within_radius():
+    """Tests stations_within_radius"""
+
+    A = MonitoringStation('idA', 'measureA', 'A', (0, 0), (0, 1), 'riverA', 'townA')
+    B = MonitoringStation('idB', 'measureB', 'B', (1, 1), (0, 1), 'riverB', 'townB')
+    C = MonitoringStation('idC', 'measureC', 'C', (-2, -2), (0, 1), 'riverC', 'townC')
+
+    stations = [A, B, C]
+    p = (0, 0)
+    r = 2000
+
+    result = geo.stations_within_radius(stations, p, r)
+    assert A in result
+    assert B in result
+    assert C not in result
--- test_station.py
+++ test_station.py
@@ -25,3 +25,57 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+
+def test_typical_range_consistent():
+
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    river = "River X"
+    town = "My Town"
+
+    # Test consistent range
+    trange1 = (-2.3, 3.4445)
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange1, river, town)
+    assert s1.typical_range_consistent() == True
+
+    # Test inconsistent range
+    trange2 = (3.4445, -2.3)
+    s2 = MonitoringStation(s_id, m_id, label, coord, trange2, river, town)
+    assert s2.typical_range_consistent() == False
+
+    # Test None range
+    trange3 = None
+    s3 = MonitoringStation(s_id, m_id, label, coord, trange3, river, town)
+    assert s3.typical_range_consistent() == False
+
+
+def test_inconsistent_typical_range_stations():
+
+    # Create stations
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    river = "River X"
+    town = "My Town"
+
+    trange1 = (-2.3, 3.4445)
+    s1 = MonitoringStation(s_id, m_id, label, coord, trange1, river, town)
+
+    trange2 = (3.4445, -2.3)
+    s2 = MonitoringStation(s_id, m_id, label, coord, trange2, river, town)
+
+    trange3 = None
+    s3 = MonitoringStation(s_id, m_id, label, coord, trange3, river, town)
+
+    stations = [s1, s2, s3]
+
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+
+    assert s1 not in inconsistent_stations
+    assert s2 in inconsistent_stations
+    assert s3 in inconsistent_stations
